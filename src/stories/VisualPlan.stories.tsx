21f454f2239f0e1822c9f1f94ae4e7a2706817c1
Suhaha <jklopsdfw@gmail.com>
2022-08-03 10:46:31 +0800

    Set up the storybook playground (#3)
    * feat(build): use npm & storybook
    
    * init: storybook playground

---
+++ src/stories/VisualPlan.stories.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+import { VisualPlan } from '../index'
+
+export default {
+  title: 'Example/VisualPlan',
+  Component: VisualPlan,
+}
+
+export const Basic = () => <VisualPlan />
