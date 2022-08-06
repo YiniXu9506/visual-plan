b491eaaca64a16ef413089c9f4713838b82292be
YiniXu9506 <xuyini@pingcap.com>
2022-08-06 23:47:43 +0800

    chore: update visual plan introduce

--- .storybook/stories/VisualPlan.stories.tsx
+++ .storybook/stories/VisualPlan.stories.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import { VisualPlan } from '../../src/VisualPlan'
+import VisualPlan from '../../src/VisualPlan'
 import mockData from './vp_mock.json'
 
 export default {
@@ -9,5 +9,7 @@ export default {
 }
 
 export const Basic = () => (
-  <VisualPlan data={mockData as any} viewport={{ height: 500, width: 1000 }} />
+  <div style={{ height: 600 }}>
+    <VisualPlan data={mockData as any} />
+  </div>
 )
